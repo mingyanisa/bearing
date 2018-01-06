b3c2d7b5c7aed245af5a575611d105291b3d9d44
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2018-01-06 22:06:10 +0700

    Add http for socket io

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var fs = require('fs-extra');
 var sql = require('mysql');
-var io = require('socket.io')(http);
 
-app.listen(3000);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -25,7 +22,15 @@ app.use(function (req, res, next) {
 
 app.set('view engine', 'pug')
 
-io.on('connection', (socket) => {
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+http.listen(3000, function () {
+    console.log('Listening on port 3000');
+});
+
+io.on('connection', socket => {
+    console.log('Socket io a user connected');
     console.log('ming');
 });
 
